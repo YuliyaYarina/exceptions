362587f7d3c07486f304659e83ea1c8c3b6ffb1e
Юлия <Yulechka140200@gmail.com>
2023-08-05 13:17:23 +0500

    hw12

--- src/Validator.java
+++ src/Validator.java
@@ -4,24 +4,22 @@ import java.util.regex.Pattern;
 public class Validator {
 
 private final static Pattern regex = Pattern.compile("[A-Za-z0-9_]{0,20}");
-    public static void validateWithRegex(String login, String password, String confirmPassword) {
-        throws Exception{
+    public static void validateWithRegex(String login, String password, String confirmPassword)
+        throws Exception {
 
-            if (Objects.isNull(login) || Objects.isNull(password)) {
-                throw new Exception();
-            }
-
-            if (!login.matches(regex.pattern())) {
-                throw new WrongLoginException("Логин не должен быть больше 20 символов и должен состоять из латинских символов, чисел и нижнего подчеркивания");
-            }
-            if (!password.matches(regex.pattern())){
-                throw new WrongPasswordException("Логин не должен быть больше 20 символов и должен состоять из латинских символов, чисел и нижнего подчеркивания");
-            }
-            if (!password.equals(confirmPassword)){
-                throw new WrongPasswordException("Пароли не совпадают");
-            }
-
+        if (Objects.isNull(login) || Objects.isNull(password)) {
+            throw new Exception();
         }
 
+        if (!login.matches(regex.pattern())) {
+            throw new WrongLoginException("Логин не должен быть больше 20 символов и должен состоять из латинских символов, чисел и нижнего подчеркивания");
+        }
+        if (!password.matches(regex.pattern())) {
+            throw new WrongPasswordException("Логин не должен быть больше 20 символов и должен состоять из латинских символов, чисел и нижнего подчеркивания");
+        }
+        if (!password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Пароли не совпадают");
+        }
     }
+
 }
